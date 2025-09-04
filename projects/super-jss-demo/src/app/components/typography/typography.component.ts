45e307dc36017398a69e3368850092b37fad2232
Ricardo Santoyo <ricardo.santoyo@hotmail.com>
2025-09-03 23:49:49 -0400

    improved typography definitions

--- projects/super-jss-demo/src/app/components/typography/typography.component.ts
+++ projects/super-jss-demo/src/app/components/typography/typography.component.ts
@@ -8,26 +8,26 @@ import { sjBorderShadow, sjCard } from '../../sjStyling/sjStyles';
   imports: [CommonModule, SjDirective],
   template: `
     <div [sj]="{ d: 'flex', fxDir: 'column', w: '100%' }">
-      <h3 [sj]="{ c: 'primary'}">Typography:</h3>
+      <h3 [sj]="{}">Typography:</h3>
       <div [sj]="[sjCard({ fxDir: 'column' })]">
-        <h1 [sj]="{}">
-          H1:
-          <a href="https://www.npmjs.com/package/super-jss">Super JSS</a> leaps
-        </h1>
-        <h2 [sj]="{ c: 'tertiary' }">H2: Over lazy CSS colleagues</h2>
-        <h3 [sj]>H3: No media query, thanks to TypeScript!</h3>
-        <h4 [sj]>H4: Just xs || md || lg || xl, Yarn's got our back!</h4>
-        <h5 [sj]>H5: Bold and bolder, Angular style!</h5>
-        <h6 [sj]>H6: Fonts rocks in this project!</h6>
-        <p [sj]>P: Theming too, making our text look great!</p>
-        <span [sj]>SPAN: Flexing flex, for a flexible layout!</span>
-        <strong [sj]>STRONG: SJSS wins with its responsive design!</strong>
+        <h1 [sj]="{ c: 'primary' }">H1: {{ sampleText }}</h1>
+        <h2 [sj]="{ c: 'secondary' }">H2: {{ sampleText }}</h2>
+        <h3 [sj]="{ c: 'tertiary' }">H3: {{ sampleText }}</h3>
+        <h4 [sj]>H4: {{ sampleText }}</h4>
+        <h5 [sj]>H5: {{ sampleText }}</h5>
+        <h6 [sj]>H6: {{ sampleText }}</h6>
+        <p [sj]>P: {{ sampleText }}{{ sampleText }}{{ sampleText }}</p>
+        <span [sj]>SPAN: {{ sampleText }}{{ sampleText }}{{ sampleText }}</span>
+        <strong [sj]>STRONG: {{ sampleText }}{{ sampleText }}{{ sampleText }}</strong>
         <body [sj]>
-          BODY: SJSS is the best!
+          BODY: {{ sampleText }}{{ sampleText }}{{ sampleText }}
         </body>
         <caption [sj]>
-          CAPTION: SJSS is the best!
+          CAPTION: {{ sampleText }}
         </caption>
+        <small [sj]>
+          SMALL: {{ sampleText }}
+        </small>
       </div>
     </div>
   `,
@@ -35,4 +35,5 @@ import { sjBorderShadow, sjCard } from '../../sjStyling/sjStyles';
 export class TypographyComponent {
   protected readonly sjBorderShadow = sjBorderShadow;
   protected readonly sjCard = sjCard;
+  protected readonly sampleText = 'The quick brown fox jumps over the lazy dog. '; // New variable
 }
--- projects/super-jss/src/lib/themes/shared-options/typography.ts
+++ projects/super-jss/src/lib/themes/shared-options/typography.ts
@@ -2,21 +2,22 @@ import { SjTypography } from '../../models/interfaces';
 
 export const DEFAULT_TYPOGRAPHY: SjTypography = {
   default: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: 'system-ui, "Helvetica", "Arial", sans-serif',
     fontSize: 1,
-    lineHeight: 1.3,
-    transition: 'all 0.2s ease-in-out', // Added transition
+    lineHeight: { xs: 1.3, md: 1.6 }, 
+    transition: 'all 0.1s ease-in-out',
+    color: 'dark.dark', //
   },
-  H1: { fontSize: { xs: 2.5, md: 4 }, fontWeight: '800', lineHeight: 2, margin:0,  marginBlockEnd: 2, marginBlockStart: 2  },
-  H2: { fontSize: { xs: 2, md: 3.2 }, fontWeight: '600', lineHeight: 1.5, marginBlockEnd: 1.8, marginBlockStart: 1.8  },
-  H3: { fontSize: { xs: 1.75, md: 2.7 }, fontWeight: '600', lineHeight: 1.4, marginBlockEnd: 1.5, marginBlockStart: 1.5  },
-  H4: { fontSize: { xs: 1.5, md: 2 }, fontWeight: '600', lineHeight: 1.3 , marginBlockEnd: 0.8, marginBlockStart: 0.8  },
-  H5: { fontSize: { xs: 1.25, md: 1.75 }, fontWeight: '600', lineHeight: 1.2 , marginBlockEnd: 0.6, marginBlockStart: 0.6  },
-  H6: { fontSize: { xs: 1, md: 1.25 }, fontWeight: '600', lineHeight: 1.2 , marginBlockEnd: 0.4, marginBlockStart: 0.4  },
-  P: { fontSize: 1, fontWeight: 'normal', lineHeight: 1.6 , marginBlockEnd: 0.8, marginBlockStart: 0.4  },
-  SPAN: { fontSize: 0.9, fontWeight: 'normal', lineHeight: 1.2, marginBlockEnd: 0.2, marginBlockStart: 0.2  },
-  STRONG: { fontSize: 1, fontWeight: 'bold', lineHeight: 1.2, marginBlockEnd: 0.2, marginBlockStart: 0.2 },
-  BODY: { fontSize: 1, fontWeight: 'normal', lineHeight: 1.6 , marginBlockEnd: 0.5, marginBlockStart: 0.2  },
-  CAPTION: { fontSize: 0.8, fontWeight: 'normal', lineHeight: 1.2, marginBlockEnd: 0.2, marginBlockStart: 0.2 },
-  SMALL: { fontSize: 0.75, fontWeight: 'normal', lineHeight: 1.2, marginBlockEnd: 0.15, marginBlockStart: 0.15  },
+  H1: { fontSize: { xs: 2.5, md: 4 }, fontWeight: '800', lineHeight: { xs: 2.3, md: 3.5 }, marginBlockEnd: 2, marginBlockStart: 2 },
+  H2: { fontSize: { xs: 2, md: 3.2 }, fontWeight: '600', lineHeight: { xs: 1.75, md: 3.2 }, marginBlockEnd: 1.8, marginBlockStart: 1.8 },
+  H3: { fontSize: { xs: 1.75, md: 2.7 }, fontWeight: '600', lineHeight: { xs: 1.8, md: 3 }, marginBlockEnd: 1.5, marginBlockStart: 1.5 },
+  H4: { fontSize: { xs: 1.5, md: 2 }, fontWeight: '600', lineHeight: { xs: 1.3, md: 1.6 }, marginBlockEnd: 0.8, marginBlockStart: 0.8 },
+  H5: { fontSize: { xs: 1.25, md: 1.75 }, fontWeight: '600', lineHeight: { xs: 1.4, md: 2 }, marginBlockEnd: 0.4, marginBlockStart: 0.4 },
+  H6: { fontSize: { xs: 1, md: 1.25 }, fontWeight: '600', lineHeight: { xs: 1.2, md: 1.4 }, marginBlockEnd: 0.4, marginBlockStart: 0.4 },
+  P: { fontSize: 1, fontWeight: 'normal', lineHeight: { xs: 1.6, md: 1.8 }, marginBlockEnd: 0.8, marginBlockStart: 0.4 },
+  SPAN: { fontSize: 0.9, fontWeight: 'normal', lineHeight: { xs: 1.2, md: 1.4 }, marginBlockEnd: 0.2, marginBlockStart: 0.2 },
+  STRONG: { fontSize: 1, fontWeight: 'bold', lineHeight: { xs: 1.2, md: 1.4 }, marginBlockEnd: 0.2, marginBlockStart: 0.2 },
+  BODY: { fontSize: 1, fontWeight: 'normal', lineHeight: { xs: 1.6, md: 1.8 }, marginBlockEnd: 0.5, marginBlockStart: 0.2 },
+  CAPTION: { fontSize: 0.8, fontWeight: 'normal', lineHeight: { xs: 1.2, md: 1.4 }, marginBlockEnd: 0.2, marginBlockStart: 0.2 },
+  SMALL: { fontSize: 0.75, fontWeight: 'normal', lineHeight: { xs: 1.2, md: 1.4 }, marginBlockEnd: 0.15, marginBlockStart: 0.15 },
 };
