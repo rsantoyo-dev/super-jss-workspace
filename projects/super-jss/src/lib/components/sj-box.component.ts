b9f416f2679d624c6ff83d68dc0ff190d3f2cb68
Ricardo Santoyo <ricardo.santoyo@hotmail.com>
2025-09-25 15:48:15 -0400

    Simplifies SjBox component
    Refactors the SjBox component to directly use the `sjBox` function in the template.
    
    This removes the `OnChanges` lifecycle hook and the need to build a producer function, resulting in a simpler and more efficient component.

--- projects/super-jss/src/lib/components/sj-box.component.ts
+++ projects/super-jss/src/lib/components/sj-box.component.ts
@@ -6,50 +6,11 @@ import { SjStyle } from '../models/interfaces';
 @Component({
   selector: "sj-box",
   standalone: true,
-  template: `<sj-host [sj]="boxProducer"><ng-content></ng-content></sj-host>`,
+  template: `<sj-host [sj]="sjBox()"><ng-content></ng-content></sj-host>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [SjHostComponent],
 })
-export class SjBoxComponent implements OnChanges {
-  // Display mode: flex row, flex column, or grid
-  @Input() display: SjStyle['d'];
+export class SjBoxComponent  {
+  sjBox = sjBox
 
-  // Flexbox controls
-  @Input() align?: SjStyle['fxAItems'];       // align-items
-  @Input() justify?: SjStyle['fxJustify'];     // justify-content
-  @Input() wrap?: SjStyle['fxWrap'];           // flex-wrap
-  @Input() gap?: SjStyle['gap'];               // gap between children
-
-  // Colors
-  @Input() bg?: SjStyle['bg'];                 // background color
-  @Input() color?: SjStyle['c'];               // text color
-
-  // The function passed to SjDirective (producer pattern)
-  boxProducer: () => SjStyle = sjBox;
-
-  ngOnChanges(_: SimpleChanges): void {
-    this.boxProducer = this.buildProducer();
-  }
-
-  private buildProducer(): () => SjStyle {
-    const parts: Array<Partial<SjStyle>> = [];
-
-    // Display selection
-    if (this.display === 'row') parts.push(sjBox.row());
-    else if (this.display === 'column') parts.push(sjBox.column());
-    else if (this.display === 'grid') parts.push(sjBox.grid());
-
-    // Flex controls
-    if (this.align !== undefined) parts.push({ fxAItems: this.align } as Partial<SjStyle>);
-    if (this.justify !== undefined) parts.push({ fxJustify: this.justify } as Partial<SjStyle>);
-    if (this.wrap !== undefined) parts.push({ fxWrap: this.wrap } as Partial<SjStyle>);
-    if (this.gap !== undefined) parts.push({ gap: this.gap } as Partial<SjStyle>);
-
-    // Colors
-    if (this.bg !== undefined) parts.push({ bg: this.bg } as Partial<SjStyle>);
-    if (this.color !== undefined) parts.push({ c: this.color } as Partial<SjStyle>);
-
-    // Compose selected variants on top of base box; returns a BoxBuilder (producer)
-    return sjBox.with(...parts);
-  }
 }
